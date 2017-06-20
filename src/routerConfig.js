d464b10ff301072f8a276a8e7c45100cf5619786
linshao <601267015@qq.com>
2017-06-20 21:17:05 +0800

    fix router

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -1,7 +1,10 @@
-import Power from './compoments/power/power.vue'
-import Region from './compoments/region/region.vue'
-import User from './compoments/user/user.vue'
-import Tea from './compoments/region/1.vue'
+import Power from "./compoments/power/power.vue";
+import Region from "./compoments/region/region.vue";
+import User from "./compoments/user/user.vue";
+import Role from "./compoments/role/role.vue";
+import Group from "./compoments/region/group.vue";
+import Village from "./compoments/village/village.vue";
+import VillageDetail from "./compoments/village/villageDetail.vue";
 
 export default[
     {
