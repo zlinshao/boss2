870cde42cb83ee1f34f07f84f823f521d9090c35
linshao <601267015@qq.com>
2017-06-29 18:36:08 +0800

    area

--- src/App.vue
+++ src/App.vue
@@ -33,22 +33,16 @@
             HeaderVue
         },
         created (){
-            this.$http.get('staff/info').then((res) => {
-                if(res.data.code === 80019){
-                    window.location.href = 'login.html'
-                }else{
-                    globalConfig.urlName = res.data.name;
-                    this.urlName = res.data.name;
-                    this.urlCard = res.data.avatar;
-
-                }
-            });
-//            let url = window.location.href;
-//            let url_name = url.split("?")[1].split("&")[0].split("=")[1];
-//            let url_card = url.split("?")[1].split("&")[1].split("=")[1];
-//            this.urlName = decodeURIComponent(url_name);
-//            globalConfig.urlName = this.urlName;
-//            this.urlCard = decodeURIComponent(url_card);
+//            this.$http.get('staff/info').then((res) => {
+//                if(res.data.code === 80019){
+//                    window.location.href = 'login.html'
+//                }else{
+//                    globalConfig.urlName = res.data.name;
+//                    this.urlName = res.data.name;
+//                    this.urlCard = res.data.avatar;
+//
+//                }
+//            });
         }
     }
 </script>
