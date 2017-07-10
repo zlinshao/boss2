053bb11d225139709c1e9d18eae3e48b85b20f27
aigle <840207473@qq.com>
2017-07-10 11:57:28 +0800

    remind

--- src/compoments/messageCenter/messageCenter.vue
+++ src/compoments/messageCenter/messageCenter.vue
@@ -15,29 +15,29 @@
                             </div>
                         </div>
                         <ul class="inbox-nav inbox-divider">
-                            <li>
+                            <li @click="System">
                                 <a href="#">
                                     <i class="fa fa-volume-up"></i> 系统公告
                                 </a>
                             </li>
-                            <li>
+                            <li @click="Examine">
                                 <a href="#">
                                     <i class="fa fa-user"></i> 审批提醒
                                 </a>
                             </li>
-                            <li>
+                            <li @click="Substitute">
                                 <a href="#">
                                     <i class="fa fa-bell"></i> 代办提醒
                                 </a>
                             </li>
-                            <li>
+                            <li @click="Secretary">
                                 <a href="#">
                                     <i class=" fa fa-github"></i> boss小秘书
                                 </a>
                             </li>
                             <li>
                                 <a href="#">
-                                    <i class=" fa  fa-heart"></i> 收藏
+                                    <i class=" fa fa-heart"></i> 收藏
                                 </a>
                             </li>
                         </ul>
@@ -45,10 +45,10 @@
                     <aside class="lg-side">
                         <div class="inbox-head">
                             <h3>
-                                <i class="fa fa-volume-up"></i>&nbsp;&nbsp; 系统公告
+                                <i :class="[fa, font]"></i>&nbsp;&nbsp; {{message}}
                             </h3>
                             <form class="pull-right position" action="#">
-                                <div class="input-append">
+                                <div class="input-append" v-if="isSystem">
                                     <button type="button" class="btn btn-primary" @click="addAnnouncement">
                                         <i class="fa fa-plus-square"></i> 添加公告
                                     </button>
@@ -56,7 +56,8 @@
                             </form>
                         </div>
                         <div class="inbox-body panel table table-responsive">
-                            <table class="table table-striped table-advance table-hover">
+                            <!--系统公告-->
+                            <table class="table table-striped table-advance table-hover" v-if="isSystem">
                                 <thead>
                                     <tr>
                                         <th>发布时间</th>
@@ -74,7 +75,7 @@
                                         <td>《年会》</td>
                                         <td>请大家今天9:30在创意中央总部集合</td>
                                         <td>2017/07/09</td>
-                                        <td>
+                                        <td  @click.stop="isCollect">
                                             <i class=" fa fa-heart" v-if="false" style="color: #e4393c"></i>
                                             <i class=" fa fa-heart-o"></i>
                                         </td>
@@ -234,6 +235,82 @@
                                     </tr>
                                 </tbody>
                             </table>
+                            <!--审批提醒-->
+                            <table class="table table-striped table-advance table-hover" v-if="isExamine">
+                                <thead>
+                                <tr>
+                                    <th>申请时间</th>
+                                    <th>申请人</th>
+                                    <th>类别</th>
+                                    <th>审批项目</th>
+                                    <th>审批人</th>
+                                    <th>审批状态</th>
+                                    <th>收藏</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="unread" @click="announceDetail">
+                                    <td >2017/07/07</td>
+                                    <td>张三丰</td>
+                                    <td>《年会》</td>
+                                    <td>请大家今天9:30在创意中央总部集合</td>
+                                    <td>2017/07/09</td>
+                                    <td>
+                                        <span class="label label-success ">已通过</span>
+                                    </td>
+                                    <td @click.stop="isCollect">
+                                        <i class=" fa fa-heart" v-if="false" style="color: #e4393c"></i>
+                                        <i class=" fa fa-heart-o"></i>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <!--代办提醒-->
+                            <table class="table table-striped table-advance table-hover" v-if="isSubstitute">
+                                <thead>
+                                <tr>
+                                    <th>提醒时间</th>
+                                    <th>提醒人</th>
+                                    <th>提醒内容</th>
+                                    <th>收藏</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="unread" @click="announceDetail">
+                                    <td >2017/07/07</td>
+                                    <td>张三丰</td>
+                                    <td>请大家今天9:30在创意中央总部集合</td>
+                                    <td  @click.stop="isCollect">
+                                        <i class=" fa fa-heart" v-if="false" style="color: #e4393c"></i>
+                                        <i class=" fa fa-heart-o"></i>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <!--BOSS小秘书-->
+                            <table class="table table-striped table-advance table-hover" v-if="isSecretary">
+                                <thead>
+                                <tr>
+                                    <th>时间</th>
+                                    <th>发信人</th>
+                                    <th>类别</th>
+                                    <th>提醒内容</th>
+                                    <th>收藏</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="unread" @click="announceDetail">
+                                    <td >2017/07/07</td>
+                                    <td>张三丰</td>
+                                    <td>合同</td>
+                                    <td>请大家今天9:30在创意中央总部集合</td>
+                                    <td  @click.stop="isCollect">
+                                        <i class=" fa fa-heart" v-if="false" style="color: #e4393c"></i>
+                                        <i class=" fa fa-heart-o"></i>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </aside>
                 </div>
@@ -251,8 +328,17 @@
         components:{AnnouncementAdd,AnnouncementDetail},
         data(){
             return{
-
+                isSystem:true,
+                isExamine:false,
+                isSubstitute:false,
+                isSecretary:false,
+                message:'',
+                fa:'fa',
+                font:''
             }
+        },
+        mounted(){
+           this.System();
         },
         methods:{
             addAnnouncement(){
@@ -260,6 +346,41 @@
             },
             announceDetail(){
                 $('#announceDetail').modal('show');
+            },
+            System(){
+                this.isSystem = true;
+                this.isExamine =false;
+                this.isSubstitute=false;
+                this.isSecretary=false;
+                this.message='系统公告';
+                this.font='fa-volume-up';
+            },
+            Examine(){
+                this.isSystem = false;
+                this.isExamine =true;
+                this.isSubstitute=false;
+                this.isSecretary=false;
+                this.message='审批提醒';
+                this.font='fa-user';
+            },
+            Substitute(){
+                this.isSystem = false;
+                this.isExamine =false;
+                this.isSubstitute=true;
+                this.isSecretary=false;
+                this.message='代办提醒';
+                this.font='fa-bell';
+            },
+            Secretary(){
+                this.isSystem = false;
+                this.isExamine =false;
+                this.isSubstitute=false;
+                this.isSecretary=true;
+                this.message='BOSS小秘书';
+                this.font='fa-github';
+            },
+            isCollect(){
+                alert(1)
             }
         }
     }
