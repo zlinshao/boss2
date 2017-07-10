b4f032e1cad0aec6e042ef2b54add965477245c9
youngc <yooungc@163.com>
2017-07-10 16:50:16 +0800

    youngc is hahaha

--- src/compoments/common/largePic.vue
+++ src/compoments/common/largePic.vue
@@ -13,6 +13,7 @@
                             <div class="changePic">
                                 <div @click="prev" title="上一张"><i class="fa fa-long-arrow-left"></i></div>
                                 <div @click="rotateLeft" title="逆时针旋转"><i class="fa fa-rotate-left"></i></div>
+                                <div @click="download" title="下载图片"><i class="fa fa-download"></i></div>
                                 <div @click="rotateRight" title="顺时针旋转"><i class="fa fa-rotate-right"></i></div>
                                 <div @click="next" title="下一站"><i class="fa fa-long-arrow-right"></i></div>
                             </div>
@@ -35,18 +36,24 @@
         position: absolute;
         font-size: 0;
         background-color: rgba(0,0,0,.4);
+        /*bottom: 1.5rem;*/
         bottom: 30px;
-        /*right: 0;*/
         left: 50%;
-        margin-left: -100px;
-        border-radius: 30px;
+        /*right: 0;*/
+        /*left: 50%;*/
+        /*margin-left: -6.25rem;*/
+        margin-left: -125px;
+        border-radius: 1rem;
     }
     .changePic div{
         display: inline-block;
+        /*font-size: 2rem;*/
         font-size: 32px;
+        /*width: 2.5rem;*/
         width: 50px;
         color: #ddd;
         text-align: center;
+        /*line-height: 2.5rem;*/
         line-height: 50px;
     }
     .changePic div:hover{
@@ -67,15 +74,73 @@
 </style>
 <script>
     export default{
-        props : ['src'],
+        props : ['largePic'],
         data(){
             return {
                 current : 0,
-                index : 0
+                index : 0,
+                src : []
             }
         },
         components: {},
+        created (){
+            console.log(this.largePic);
+        },
+        updated (){
+            this.setRem();
+//            console.log(this.largePic);
+
+        },
+
+        watch :{
+            deep : true,
+            largePic(val){
+                console.log(val);
+                this.src = val[0].src;
+                this.index = val[0].i;
+            }
+        },
         methods : {
+            setRem(){
+                let browser={
+                    versions:function(){
+                        let u = navigator.userAgent, app = navigator.appVersion;
+                        return {//移动终端浏览器版本信息
+                            trident: u.indexOf('Trident') > -1, //IE内核
+                            presto: u.indexOf('Presto') > -1, //opera内核
+                            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+                            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+                            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+                            iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
+                            iPad: u.indexOf('iPad') > -1, //是否iPad
+                            webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+                        };
+                    }(),
+                    language:(navigator.browserLanguage || navigator.language).toLowerCase()
+                };
+
+                if(browser.versions.mobile || browser.versions.ios || browser.versions.android ||
+                    browser.versions.iPhone || browser.versions.iPad){
+                    (function(){
+                        let colCount = 20;
+                        //  设置列宽
+                        let colWidth =  document.documentElement.clientWidth / colCount;//;document.documentElement;
+                        //  确定基本的font-size
+                        document.querySelector('html').style.fontSize = colWidth + 'px';
+                    })();
+                    $('.changePic').css({
+                        'bottom': '1.5rem',
+                        'margin-left': '-6.25rem',
+                    });
+                    $('.changePic div').css({
+                        'font-size': '2rem',
+                        'width' : '2.5rem',
+                        'line-height' : '2.5rem'
+                    })
+                }
+            },
             rotateRight(){  // 顺时针旋转
 //                console.log(ev.currentTarget);
                 this.current = (this.current+90)%360;
@@ -90,17 +155,32 @@
             },
             prev(){
                 if (this.index == 0){
-                    this.index = this.src.length-1;
+                    this.index = this.largePic.src.length-1;
                 } else {
                     this.index--;
                 }
             },
             next(){
-                if (this.index == this.src.length-1){
+                if (this.index == this.largePic.src.length-1){
                     this.index = 0;
                 } else {
                     this.index++;
                 }
+            },
+            download(){
+                this.saveImageAs(this.largePic.src[this.index]);
+            },
+            /*savetxt(fileURL){
+                let fileURL=window.open (fileURL,"_blank","height=0,width=0,toolbar=no,menubar=no,scrollbars=no,resizable=on,location=no,status=no");
+                fileURL.document.execCommand("SaveAs");
+                fileURL.window.close();
+                fileURL.close();
+            }*/
+            saveImageAs(imgOrURL) {
+                if (typeof imgOrURL == 'object')
+                    imgOrURL = imgOrURL.src;
+                window.win = open (imgOrURL);
+                setTimeout('win.document.execCommand("SaveAs")', 500);
             }
         }
     }
