10b5c03fa4ec5ca3011d49b23b25bbdddb1fe4c8
aigle <840207473@qq.com>
2017-07-14 20:12:54 +0800

    bug

--- src/compoments/housingSupervise/houseEdit.vue
+++ src/compoments/housingSupervise/houseEdit.vue
@@ -10,7 +10,7 @@
                         <!--新增-->
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true" @click="closeModal">×</button>
-                            <h4 class="modal-title">编辑房屋{{houseEdit.house_pic}}</h4>
+                            <h4 class="modal-title">编辑房屋</h4>
                         </div>
 
                         <!--新增/编辑-->
