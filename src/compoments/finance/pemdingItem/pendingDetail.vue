82dbd51663cb81f3bdcd84c6faa8dba8786c996b
aigle <840207473@qq.com>
2017-08-03 17:07:36 +0800

    bgu

--- src/compoments/finance/pemdingItem/pendingDetail.vue
+++ src/compoments/finance/pemdingItem/pendingDetail.vue
@@ -7,19 +7,19 @@
             </li>
             <li>结算详情</li>
         </ol>
-
-        <section class="panel head">
-            <div class="panel-body">
-                <div>
-                    <span>合同编号</span>
-                    <span>LJS081740</span>
-                    <span :class="{'status':true,'btn':true}">带结清</span>
-                </div>
-                <div class="pull-right">
-                    <button class="btn btn-primary">生成应收款项</button>
-                </div>
+        <div class="title clearFix">
+            <div class="contractNum" style="margin-bottom: 10px">
+                <!--<h5 style="margin-right: 15px">-->
+                    <!--合同编号&emsp;-->
+                <!--</h5>-->
+                <span class="label label-primary" v-if="myDictionary.account_pending_status !==undefined">
+                    {{myDictionary.account_pending_status[account_pending_status]}}
+                </span>
             </div>
-        </section>
+            <div class="pull-right dropdown">
+                <button class="btn btn-primary pull-right">生成应收款项</button>
+            </div>
+        </div>
 
         <section class="panel">
             <div class="panel-body">
@@ -30,39 +30,39 @@
                 </header>
                 <div class="panel-body table-responsive client_info">
                     <div>
-                        <div class="col-md-12">
+                        <div class="col-md-12"v-for="item in pendingDetailList">
                             <div class="col-md-4">
-                                <div><span class="text-primary">客户姓名：</span><span>dfsdf</span></div>
-                                <div><span class="text-primary">房屋地址：</span><span>dfsdf</span></div>
-                                <div><span class="text-primary">合同编号：</span><span>dfsdf</span></div>
-                                <div><span class="text-primary">事项类型：</span><span>dfsdf</span></div>
-                                <div><span class="text-primary">入住周期：</span><span>dfsdf</span></div>
-                                <div><span class="text-primary">付款方式：</span><span>dfsdf</span></div>
-                                <div><span class="text-primary">月单价：</span><span>dfsdf</span></div>
-                                <div><span class="text-primary">往来记录：</span><span>dfsdf</span></div>
-                                <div><span class="text-primary">差额：</span><span>dfsdf</span></div>
+                                <div><span class="text-primary">客户姓名：</span><span>{{item.customer_name}}</span></div>
+                                <div><span class="text-primary">房屋地址：</span><span>{{item.detailed_address}}</span></div>
+                                <div><span class="text-primary">合同编号：</span><span></span></div>
+                                <div>
+                                    <span class="text-primary">事项类型：</span>
+                                    <span>{{myDictionary.item_type[item.item_type]}}</span>
+                                </div>
+                                <div><span class="text-primary">合同开始时间：</span><span>{{item.start_date}}</span></div>
+                                <div><span class="text-primary">合同结束时间：</span><span>{{item.end_date}}</span></div>
+                                <div>
+                                    <span class="text-primary">付款方式：</span>
+                                    <span></span>
+                                </div>
+                                <div><span class="text-primary">月单价：</span><span></span></div>
+                                <div><span class="text-primary">往来记录：</span><span></span></div>
                             </div>
                             <div class="col-md-4">
-                                <div><span class="text-primary">水费：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">电费：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">燃气费：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">物业费：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">网络费：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">转租费用：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">管理费：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">物业校验：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">违约金：</span><span>sdfdsf</span></div>
-                            </div>
-                            <div class="col-md-4">
-                                <div><span class="text-primary">应退：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">实际扣款：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">实际退款：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">结算方式：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">结算账户：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">签约人：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">所属部门：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">收款人：</span><span>sdfdsf</span></div>
-                                <div><span class="text-primary">结算日期：</span><span>sdfdsf</span></div>
+                                <div><span class="text-primary">应退：</span><span>{{item.refund_should}}</span></div>
+                                <div><span class="text-primary">实际扣款：</span><span>{{item.refund_diff}}</span></div>
+                                <div><span class="text-primary">实际退款：</span><span>{{item.refund_real}}</span></div>
+                                <div>
+                                    <span class="text-primary">结算方式：</span>
+                                    <span>{{myDictionary.payment[item.account_type]}}</span>
+                                </div>
+                                <div><span class="text-primary">结算账户：</span><span>
+                                    {{item.account_num}}
+                                </span></div>
+                                <div><span class="text-primary">签约人：</span><span>{{item.staff_name}}</span></div>
+                                <div><span class="text-primary">所属部门：</span><span>{{item.department_name}}</span></div>
+                                <div><span class="text-primary">收款人：</span><span></span></div>
+                                <div><span class="text-primary">结算日期：</span><span>{{item.settle_date}}</span></div>
                             </div>
                         </div>
                     </div>
@@ -85,49 +85,9 @@
         components: {Status},
         data(){
             return {
-                id : '',
-                currentIndex : -1,
-
-                showOper: [],
-
-                times : [
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                ],
-                moreTime : [
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                    '2017/09/12',
-                ],
+                myRentingId : '',
+                pendingDetailList : [],
+                myDictionary :[],
                 info:{
                     //成功状态 ***
                     state_success: false,
@@ -138,46 +98,35 @@
                     //失败信息 ***
                     error: ''
                 },
+                account_pending_status : '',
             }
         },
-        mounted (){
-            this.id = this.$route.query.payId;
-            for (let i = 0 ; i<this.moreTime.length ; i++){
-                this.showOper.push(false);
-            }
-//            this.times = this.moreTime;
-//            console.log(this.moreTime)
-//            console.log(this.showOper)
+        mounted(){
+            this.myRentingId = this.$route.query.RentingId;  //路由接受合同id
+            this.getDictionary();
         },
         methods : {
-            oper(){
-                $('#edit').modal('show');
+            getDictionary(){
+                this.$http.get('revenue/glee_collect/dict').then((res) =>{
+                    this.myDictionary = res.data;
+                    console.log(this.myDictionary);
+                    this.getRentingDetail();
+                })
             },
-            getHouse(data){},
-            getClient(data){},
+            getRentingDetail(){
+                this.$http.get('account/pending/' + this.myRentingId).then((res) =>{
+                    if(res.data.code === '18800'){
+                        this.account_pending_status = res.data.data.status;
+                        this.pendingDetailList .push(res.data.data);
+                        console.log(this.pendingDetailList)
+                    }else {
+                        this.pendingDetailList = [];
+                        this.info.error =res.data.msg;
+                        //显示成功弹窗 ***
+                        this.info.state_error = true;
+                    }
 
-            changeShow(index){
-//                console.log(this.currentIndex);
-                if (this.currentIndex==-1){
-                    this.currentIndex = index;
-//                this.showOper[index] = true;
-                    this.showOper.splice(index,1,true);
-//                    console.log(this.showOper)
-                }
-//                console.log(this.currentIndex);
-
-            },
-            operTime(index){
-                this.currentIndex = -1;
-                this.showOper.splice(index,1,false);
-//                console.log(this.moreTime);
-                this.times.splice(index,1,this.moreTime[index]);
-            },
-            cancel(index){
-                this.currentIndex = -1;
-                this.showOper.splice(index,1,false);
-                this.moreTime.splice(index,1,this.times[index]);
-//                console.log(this.moreTime);
+                })
             }
         }
     }
@@ -223,5 +172,22 @@
     .form-horizontal .form-group span{
         line-height: 34px;
         font-size: 14px;
+    }
+
+    .title{
+        background-color: white;
+        border-radius: 5px;
+        padding: 15px 12px;
+        margin-bottom: 15px;
+    }
+    .title>div{
+        display: inline-block;
+    }
+    .contractNum{
+    }
+    .contractNum h5{
+        display: inline-block;
+        font-weight: 600;
+        vertical-align: middle;
     }
 </style>
--- src/compoments/finance/pemdingItem/pendingItem.vue
+++ src/compoments/finance/pemdingItem/pendingItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <ol class="breadcrumb">
-            <li>财务账本{{params.search}}</li>
+            <li>财务账本</li>
             <li class="active">待处理项</li>
         </ol>
         <section class="panel">
@@ -9,13 +9,13 @@
                 <div v-show="operId==0">
                     <form class="form-inline" v-show="operId==0" role="form">
                         <div class="pro-sort">
-                            <select name="" class="form-control"  v-model="params.type" >
+                            <select name="" class="form-control" @change="searchList"  v-model="params.event_type" >
                                 <option value="">事件类型</option>
                                 <option :value="key" v-for="(value,key) in dictionary.item_type">{{value}}</option>
                             </select>
                         </div>
                         <div class="pro-sort">
-                            <select name="" class="form-control" v-model="params.collect_rent">
+                            <select name="" class="form-control" @change="searchList" v-model="params.collect_rent">
                                 <option value="">收租类型</option>
                                 <option :value="key" v-for="(value,key) in dictionary.collect_rent">{{value}}</option>
                             </select>
@@ -27,31 +27,28 @@
 
                         <div class="input-group">
                             <!--<label class="sr-only" for="search_info">搜索</label>-->
-                            <input type="text" class="form-control" placeholder="房屋地址/租房人" v-model="params.search">
+                            <input type="text" class="form-control" @keyup="searchList" placeholder="房屋地址/租房人" v-model="params.search">
                             <span class="input-group-btn">
                                 <button class="btn btn-success" id="search" type="button" @click="" v-model="params.search">
                                     搜索
                                 </button>
                             </span>
                         </div>
-                        <div class="form-group pull-right">
-                            <a class="btn btn-success" data-toggle="modal" data-target="#add">
-                                <i class="fa fa-plus-square"></i>&nbsp;新增待处理项
-                            </a>
-                        </div>
+                        <!--<div class="form-group pull-right">-->
+                            <!--<a class="btn btn-success" data-toggle="modal" data-target="#add">-->
+                                <!--<i class="fa fa-plus-square"></i>&nbsp;新增待处理项-->
+                            <!--</a>-->
+                        <!--</div>-->
                     </form>
                 </div>
 
                 <div v-show="operId!=0" class="col-lg-12 remind">
                     <ul>
                         <li><h5><a>已选中&nbsp;1&nbsp;项</a></h5></li>
-                        <li>
-                            <h5 @click="edit"><a><i class="fa fa-pencil"></i>&nbsp;编辑</a></h5>
-                        </li>
-                        <li>
+                        <li @click="deletePending">
                             <h5><a><i class="fa fa-times-circle-o"></i>删除</a></h5>
                         </li>
-                        <li>
+                        <li  @click="settleAccount">
                             <h5><a><i class="fa fa-calculator"></i>结算</a></h5>
                         </li>
                     </ul>
@@ -80,25 +77,32 @@
                             <th class="text-center">结算账户</th>
                             <th class="text-center">结算人</th>
                             <th class="text-center">状态</th>
+                            <th class="text-center">详情</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr class="text-center" v-for="item in pendingList">
-                            <td><input type="checkbox" @click="picked(item,$event)"></td>
+                            <td><input type="checkbox":value="item.id" :checked="operId===item.id"
+                                       @click="picked(item,$event)"></td>
                             <td>{{dictionary.item_type[item.item_type]}}</td>
                             <td>{{}}</td>
-                            <td>{{}}</td>
-                            <td>{{}}</td>
-                            <td>{{}}</td>
+                            <td>{{item.staff_name}}</td>
+                            <td>{{item.detailed_address}}</td>
+                            <td>{{item.customer_name}}</td>
                             <td>{{item.start_date}}</td>
                             <td>{{item.end_date}}</td>
-                            <td>{{item.penalty_fee}}</td>
-                            <td>{{}}</td>
-                            <td>{{}}</td>
-                            <td></td>
-                            <td>{{}}</td>
+                            <td>{{item.refund_should}}</td>
+                            <td>{{item.refund_diff}}</td>
+                            <td>{{item.refund_real}}</td>
+                            <td>{{item.account_num}}</td>
+                            <td>{{item.operator_name}}</td>
                             <td>
-                                <label class="label label-warning">{{dictionary.account_pending_status [item.status]}}</label>
+                                <label class="label label-warning">{{dictionary.account_pending_status[item.status]}}</label>
+                            </td>
+                            <td>
+                                <router-link :to="{path:'/pendingDetail',query: {RentingId: item.id}}">
+                                    详情
+                                </router-link>
                             </td>
                         </tr>
                         <tr v-if="isShow">
@@ -113,12 +117,15 @@
         </div>
 
         <!--新增模态框-->
-        <addModal></addModal>
+        <!--<addModal></addModal>-->
 
         <Page :pg="paging" @pag="getPage" :beforePage = 'params.page'></Page>
 
         <!--提示信息-->
         <Status :state='info'></Status>
+
+        <settleModal :settleId = 'operId' :dictionary = 'dictionary' :collect_rent = 'collect_rent'></settleModal>
+        <Confirm :msg="confirmMsg" @yes="getConfirm"></Confirm>
     </div>
 </template>
 <script>
@@ -126,20 +133,23 @@
     import Status from '../../common/status.vue';
     import DatePicker from '../../common/datePicker.vue'
 
-    import addModal from './pendingAdd.vue'
+//    import addModal from './pendingAdd.vue'
+    import settleModal from './pendingSettle.vue'
+    import Confirm from '../../common/confirm.vue'
 
     export default{
-        components: {Page,Status,DatePicker,addModal},
+        components: {Page,Status,DatePicker,settleModal ,Confirm},
         data(){
             return {
                 operId : 0,
+                collect_rent :'',
                 paging : 1,
                 dictionary : [],
                 pendingList : [],
                 isShow:false,
                 params : {
                     page : 1,
-                    type : '',
+                    event_type : '',
                     range : '',
                     search : '',
                     collect_rent : '',
@@ -161,7 +171,8 @@
                     success: '',
                     //失败信息 ***
                     error: ''
-                }
+                },
+                confirmMsg:[],
             }
         },
         created () {
@@ -172,31 +183,74 @@
                 this.$http.get('revenue/glee_collect/dict').then((res) =>{
                     this.dictionary = res.data;
                     console.log(this.dictionary);
-                    this.getPendingList();
+                    this.searchList();
                 })
+            },
+            searchList(){
+                this.params.page = 1;
+                this.getPendingList();
             },
             getPendingList(){
                 this.$http.get('account/pending',{ params : this.params}).then((res) =>{
-                    this.pendingList = res.data.data.data;
-                    console.log(this.pendingList)
+                    if(res.data.code === '18800'){
+                        this.pendingList = res.data.data.data;
+                        this.isShow = false;
+                        this.info.success =res.data.msg;
+                        //显示成功弹窗 ***
+                        this.info.state_success = true;
+                        console.log(this.pendingList)
+                    }else {
+                        this.pendingList = [];
+                        this.isShow = true;
+                        this.info.error =res.data.msg;
+                        //显示成功弹窗 ***
+                        this.info.state_error = true;
+                    }
+
                 })
             },
             picked(item,e){
                 if(e.target.checked===true){
                     this.operId = item.id;
+                    this.collect_rent = item.collect_rent;
                 }else {
                     this.operId = 0;
                 }
             },
             getPage(val){
                 this.params.page = val;
+                this.getPendingList();
             },
             edit(){
                 $('#operModal').modal('show');
             },
             getDate(val){
-                this.params.range = val
+                this.params.range = val;
+                this.searchList();
             },
+            settleAccount(){
+                $('#pendingSettle').modal('show');
+            },
+            deletePending(){
+                this.confirmMsg = {msg:'您确定删除吗'};
+                $('#confirm').modal('show');
+            },
+            getConfirm(){
+                this.$http.post('account/pending/delete/' +  this.operId).then((res) =>{
+                    if(res.data.code === '18810'){
+                        this.operId = 0;
+                        this.searchList();
+                        this.info.success =res.data.msg;
+                        //显示成功弹窗 ***
+                        this.info.state_success = true;
+                    }else {
+                        this.info.error =res.data.msg;
+                        //显示成功弹窗 ***
+                        this.info.state_error = true;
+                    }
+
+                })
+            }
         }
     }
 </script>
