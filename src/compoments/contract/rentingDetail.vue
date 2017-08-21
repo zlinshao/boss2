0b835a468d7a10ca4c17ae72a44c5fdf147b0a8b
aigle <840207473@qq.com>
2017-08-21 14:29:57 +0800

    bug

--- src/compoments/contract/rentingDetail.vue
+++ src/compoments/contract/rentingDetail.vue
@@ -375,39 +375,39 @@
                                 <div class="ownerInfo" v-if="item.relative_customer!== undefined && item.relative_customer!== null"
                                      v-for="relative in item.relative_customer">
                                     <div v-if="relative.id !== undefined">
-                                        <header>附属房东信息</header>
+                                        <header>附属租客信息</header>
                                         <div class="infoContainer clearFix">
                                             <div class="col-lg-4">
                                                 <div class="infoList">
-                                                    <span>业主姓名<sup>*</sup>：</span>
+                                                    <span>租客姓名：</span>
                                                     <span>{{relative.name}}</span>
                                                 </div>
                                                 <div class="infoList">
-                                                    <span>尊称<sup>*</sup>：</span>
+                                                    <span>尊称：</span>
                                                     <span>{{dictionary.gender[relative.gender]}}</span>
                                                 </div>
                                             </div>
                                             <div class="col-lg-4">
                                                 <div class="infoList">
-                                                    <span>国籍<sup>*</sup>：</span>
+                                                    <span>国籍：</span>
                                                     <span>
                                                     {{dictionary.nationality[relative.nationality]}}
                                                 </span>
                                                 </div>
                                                 <div class="infoList">
-                                                    <span>手机号码<sup>*</sup>：</span>
+                                                    <span>手机号码：</span>
                                                     <span>{{relative.mobile}}</span>
                                                 </div>
                                             </div>
                                             <div class="col-lg-4">
                                                 <div class="infoList">
-                                                    <span>证件类型<sup>*</sup>：</span>.
+                                                    <span>证件类型：</span>.
                                                     <span>
                                                     {{dictionary.credentials[relative.id_type]}}
                                                 </span>
                                                 </div>
                                                 <div class="infoList">
-                                                    <span>身份证号<sup>*</sup>：</span>
+                                                    <span>身份证号：</span>
                                                     <span>{{relative.id_num}}</span>
                                                 </div>
                                             </div>
