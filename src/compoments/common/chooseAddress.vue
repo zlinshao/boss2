e6fff8649ad6553c0c3dbccedfaccca024a0d718
youngc <yooungc@163.com>
2017-08-21 17:43:45 +0800

    choose address

--- src/compoments/common/chooseAddress.vue
+++ src/compoments/common/chooseAddress.vue
@@ -189,8 +189,8 @@
 <script>
     import Status from './status.vue';
 
-    let addr="//restapi.amap.com/v3/assistant/inputtips?key=181a17662347392d30ce7962d0deb60a&datatype=poi&types=120300";
-    let cityAddr = '//restapi.amap.com/v3/ip?key=181a17662347392d30ce7962d0deb60a&ip=';
+    let addr="//restapi.amap.com/v3/assistant/inputtips?key=3834fe3d734950926dbe78c69b28159f&datatype=poi&types=120300";
+    let cityAddr = '//restapi.amap.com/v3/ip?key=3834fe3d734950926dbe78c69b28159f&ip=';
     export default{
 
         data(){
@@ -246,13 +246,17 @@
                 this.$http.get(addr+'&keywords='+this.searchInfo+'&city='+this.chooseCity)
                     .then(
                         (res) => {
-                            if (res.data.tips.length!=0){
-                                this.villages = res.data.tips;
-                                this.isShow = false;
-                            } else {
-                                this.villages = [];
-                                this.isShow = true;
+//                            console.log(res)
+                            if (res.data.infocode==10000){
+                                if (res.data.tips.length!=0){
+                                    this.villages = res.data.tips;
+                                    this.isShow = false;
+                                } else {
+                                    this.villages = [];
+                                    this.isShow = true;
+                                }
                             }
+
 
                         }
                     );
