c00107fc6dc31833612d295617f8fdb7ed2a5152
youngc <yooungc@163.com>
2017-08-30 13:17:46 +0800

    header

--- src/compoments/header/header.vue
+++ src/compoments/header/header.vue
@@ -391,7 +391,7 @@
                             <!--</li>-->
                         <!--</ul>-->
                     <!--</li>-->
-                    <li class="sub-menu">
+                    <li class="sub-menu" v-show="account_book">
                     <!--<li class="sub-menu">-->
                         <a href="javascript:;">
                             <i class="fa fa-book"></i>
