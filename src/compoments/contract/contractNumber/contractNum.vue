e5fefd56f349cc01d0596854bda1d46386acb45f
youngc <yooungc@163.com>
2017-10-30 19:17:47 +0800

    remark

--- src/compoments/contract/contractNumber/contractNum.vue
+++ src/compoments/contract/contractNumber/contractNum.vue
@@ -539,7 +539,7 @@
                     this.info.error = '请填写备注信息';
                     //显示失败弹窗 ***
                     this.info.state_error = true;
-                    return
+                    return;
                 }
                 this.$http.post('/code/Contract_Number_Record/addRemark',{
                     id : this.operId,
