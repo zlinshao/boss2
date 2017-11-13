f3bd535409b5e266e8f614fadd70617e53e1c2ae
aigle <840207473@qq.com>
2017-11-13 16:02:47 +0800

    colspan

--- src/compoments/villageManage/village.vue
+++ src/compoments/villageManage/village.vue
@@ -98,7 +98,7 @@
                             </td>
                         </tr>
                         <tr v-if="isShow">
-                            <td colspan="7" class="text-center text-muted">
+                            <td colspan="8" class="text-center text-muted">
                                 <h4>暂无数据....</h4>
                             </td>
                         </tr>
