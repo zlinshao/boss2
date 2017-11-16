6579b877cc27a8120e57c398c00ae88f9868960d
linshao <601267015@qq.com>
2017-11-16 10:20:02 +0800

    data-dismiss

--- src/compoments/periodicFor/periodicDetail.vue
+++ src/compoments/periodicFor/periodicDetail.vue
@@ -52,7 +52,7 @@
                             <div class="modal-dialog ">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <button type="button" class="close" @click="leading_close">
+                                        <button type="button" class="close" data-dismiss="modal">
                                             <span>&times;</span>
                                         </button>
                                         <h4 class="modal-title">提示信息</h4>
