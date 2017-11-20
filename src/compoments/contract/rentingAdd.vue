c842b694a03d29a1015d8f7a3adae1fd283b3e70
linshao <601267015@qq.com>
2017-11-20 18:19:48 +0800

    rentingAdd

--- src/compoments/contract/rentingAdd.vue
+++ src/compoments/contract/rentingAdd.vue
@@ -161,7 +161,7 @@
                                         <div class="col-sm-3 padding_0">
                                             <label class="control-label">
                                                 <input type="checkbox" :checked="change_payType"
-                                                       @click="changePayType($event)">付款方式变化{{more_type}}
+                                                       @click="changePayType($event)">付款方式变化
                                             </label>
                                         </div>
                                     </div>
