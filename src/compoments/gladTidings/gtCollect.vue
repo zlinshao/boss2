112c3cead8026e33b56d211c4d5c1dcc6a1da17d
linshao <601267015@qq.com>
2018-01-04 20:34:31 +0800

    priod

---
+++ src/compoments/gladTidings/gtCollect.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        收房喜报
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/compoments/gladTidings/gtRent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        租房喜报
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+
+<style scoped>
+
+</style>
