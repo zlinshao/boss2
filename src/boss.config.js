58a57a8c319ba366fddb874cd6e20dcb74313667
Marvin <Marvin@locker.top>
2018-01-31 11:21:01 +0000

    compress optimize

---
+++ src/boss.config.js
@@ -0,0 +1,17 @@
+globalConfig = {
+    server: 'http://test.v2.api.boss.lejias.cn/',                       //接口地址
+    host: 'http//test.v2.boss.lejias.cn/',
+    pic_address: 'http://test.v2.api.boss.lejias.cn/picture/upload',    //图片地址
+    vedio_address: 'http://test.v2.api.boss.lejias.cn/picture/upload',  //视频地址
+    appId:'dingoaop2sz9fntxxgaytz',
+    state: 'test',
+    env: 'test',
+    urlName: '',                                                        //用户名
+};
+
+function indexOf(val) {
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] === val) return i;
+    }
+    return -1;
+}
