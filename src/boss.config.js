55350a63c1e5b32718ddae0eefa07983e5b0beab
aigle <840207473@qq.com>
2018-03-31 15:58:58 +0800

    dddd

--- src/boss.config.js
+++ src/boss.config.js
@@ -1,14 +1,15 @@
+/**
+ * Created by linshao on 2017/6/28.
+ */
 
 globalConfig = {
-    server: "http://v2.api.boss.lejias.cn/",
-    host: "http//v2.boss.lejias.cn/",
-    pic_address: "http://v2.api.boss.lejias.cn/picture/upload",
-    vedio_address: 'http://v2.api.boss.lejias.cn/picture/upload',  //视频地址
-    //appId:"dingoaop2sz9fntxxgaytz",
-    appId:"dingoa6pqll6sfxta66zjq",
-    state: 'production',
-    urlName: "",
-    env: 'production'
+    server: 'http://test.v2.api.boss.lejias.cn/',                       //接口地址
+    host: 'http//test.v2.boss.lejias.cn/',
+    pic_address: 'http://test.v2.api.boss.lejias.cn/picture/upload',    //图片地址
+    appId:'dingoaop2sz9fntxxgaytz',
+    state: 'dev',
+    env: 'dev',
+    urlName: '',                                                        //用户名
 };
 
 function indexOf(val) {
@@ -17,4 +18,3 @@ function indexOf(val) {
     }
     return -1;
 }
-
